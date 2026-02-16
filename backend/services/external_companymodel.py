d280aa2d23e4ba1a3e5060a69ae7703c9368979c
Wilson Marquina <wmjosemarquez@gmail.com>
2026-02-16 11:55:33 -0400

    saving info

--- backend/services/external_companymodel.py
+++ backend/services/external_companymodel.py
@@ -1,9 +1,1 @@
-from pydantic import BaseModel
 
-class ExternalCompany(BaseModel):
-
-    name: str | None
-    domain: str | None
-    industry: str | None
-    revenue: int | None
-    technologies: list[str] | None
